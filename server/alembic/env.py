d6607db1fa8682570a546239a74e19e996068b06
vmstarchenko <vmstarchenko@edu.hse.ru>
2023-10-07 16:22:07 +0300

    add alembic tests

--- server/alembic/env.py
+++ server/alembic/env.py
@@ -52,6 +52,8 @@ def run_migrations_offline() -> None:
         target_metadata=target_metadata,
         literal_binds=True,
         compare_type=True,
+        compare_server_default=True,
+        include_schemas=True,
         dialect_opts={"paramstyle": "named"},
     )
 
@@ -68,17 +70,22 @@ def run_migrations_online() -> None:
     """
     configuration = config.get_section(config.config_ini_section)
     configuration['sqlalchemy.url'] = get_url()
-    connectable = engine_from_config(
-        configuration,
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = context.config.attributes.get("connection", None)
+
+    if connectable is None:
+        connectable = engine_from_config(
+            configuration,
+            prefix="sqlalchemy.",
+            poolclass=pool.NullPool,
+        )
 
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
             target_metadata=target_metadata,
             compare_type=True,
+            compare_server_default=True,
+            include_schemas=True,
         )
 
         with context.begin_transaction():
--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,1 +1,7 @@
+import pytest
 from fixtures import *
+
+
+@pytest.fixture
+def alembic_config() -> dict[str, str]:
+    return {'file': 'alembic/alembic.ini'}
